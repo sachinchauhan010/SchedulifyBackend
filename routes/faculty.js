1da6d631334e1cafbf08662d5f1d8598bba2a059
sachinchauhan010 <010sssachin@gmail.com>
2024-09-19 10:54:21 +0530

    removed getDayTT route

--- routes/faculty.js
+++ routes/faculty.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { login, signup, setTimeTable } from "../controllers/facultyController.js";
+import { login, signup, setTimeTable} from "../controllers/facultyController.js";
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
 router.post('/settimetable', setTimeTable);
-router.post('/getdaytimetable', getDayTimeTable);
 
 export default router;
